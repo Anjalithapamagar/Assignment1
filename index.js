41a850200ba83f522874aa6e9d39f2e0a1c3ecd1
Anjali <anjalimagar666@gmail.com>
2023-09-25 21:55:54 -0400

    Implemented GET method

---
+++ index.js
@@ -0,0 +1,38 @@
+let SERVER_NAME = 'product-api'
+let PORT = 3000;
+let HOST = '127.0.0.1';
+
+let totalCount = 0
+
+let errors = require('restify-errors');
+let restify = require('restify')
+
+  // Get a persistence engine for the products
+  , productsSave = require('save')('products')
+  // Create the restify server
+  , server = restify.createServer({ name: SERVER_NAME})
+
+  server.listen(PORT, HOST, function () {
+  console.log('Server %s listening at %s', server.name, server.url)
+  console.log('**** Resources: ****')
+  console.log('********************')
+  console.log('/products');
+  console.log('/products/:id');
+})
+
+server.use(restify.plugins.fullResponse());
+server.use(restify.plugins.bodyParser());
+
+// Get all products in the system
+server.get('/products', function (req, res, next) {
+  console.log('GET /products params=>' + JSON.stringify(req.params));
+
+  // Find every entity within the given collection
+  productsSave.find({}, function (error, products) {
+
+    // Return all of the products in the system
+    res.send(products)
+  })
+})
+
+
